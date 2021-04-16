31676bf50e8074604135b60f6852ee7f28855dff
lubygyuri <lubigyurma@gmail.com>
2021-04-17 00:14:42 +0200

    #17 uzenetek v1.0

--- Kozossegi/Teszt/templates/uzenetek.tpl
+++ Kozossegi/Teszt/templates/uzenetek.tpl
@@ -2,6 +2,121 @@
 {include file="menu.tpl"}
 <div id="app5">
 
-    <p>Hello uzenetek</p>
+    <div class="profile-area">
+        <div class="profile-container">
+            <div class="messages-card mx-auto">
+                <div class="messages-header">
+                    <span>Example example</span>
+                    <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="profilkép">
+                </div>
+                <div class="messages-main">
+                    <div class="messages-friends-container">
+                        <ul>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example2 Example2</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor...</p>
+                                        <small>14:34</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example3 Example3</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor...</p>
+                                        <small>7:30</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example4 Example4</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor sit.</p>
+                                        <small>12:49</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example5 Example5</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor sit amet.</p>
+                                        <small>1:15</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example6 Example6</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor...</p>
+                                        <small>21:11</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example6 Example6</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor...</p>
+                                        <small>21:11</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example6 Example6</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor...</p>
+                                        <small>21:11</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example6 Example6</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor...</p>
+                                        <small>21:11</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example6 Example6</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor...</p>
+                                        <small>21:11</small>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="chat-section">
+                        <div class="chat-messages">
+                            <h2>Hello</h2>
+                        </div>
+                        <div class="chat-toolbar">
+                            Toolbar
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 
 </div>
--- Kozossegi/Teszt/templates_c/384aab2ccffe3e3fa45ec699e3298a54df090614_0.file.uzenetek.tpl.php
+++ Kozossegi/Teszt/templates_c/384aab2ccffe3e3fa45ec699e3298a54df090614_0.file.uzenetek.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-04-16 22:07:20
+/* Smarty version 3.1.39, created on 2021-04-17 00:00:34
   from 'C:\xampp\htdocs\kozossegi_oldal\Kozossegi\Teszt\templates\uzenetek.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_6079ee785db486_33906090',
+  'unifunc' => 'content_607a0902a1cd98_27703578',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '384aab2ccffe3e3fa45ec699e3298a54df090614' => 
     array (
       0 => 'C:\\xampp\\htdocs\\kozossegi_oldal\\Kozossegi\\Teszt\\templates\\uzenetek.tpl',
-      1 => 1618603623,
+      1 => 1618610431,
       2 => 'file',
     ),
   ),
@@ -22,13 +22,128 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:menu.tpl' => 1,
   ),
 ),false)) {
-function content_6079ee785db486_33906090 (Smarty_Internal_Template $_smarty_tpl) {
+function content_607a0902a1cd98_27703578 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 $_smarty_tpl->_subTemplateRender("file:menu.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 <div id="app5">
 
-    <p>Hello uzenetek</p>
+    <div class="profile-area">
+        <div class="profile-container">
+            <div class="messages-card mx-auto">
+                <div class="messages-header">
+                    <span>Example example</span>
+                    <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="profilkép">
+                </div>
+                <div class="messages-main">
+                    <div class="messages-friends-container">
+                        <ul>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example2 Example2</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor...</p>
+                                        <small>14:34</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example3 Example3</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor...</p>
+                                        <small>7:30</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example4 Example4</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor sit.</p>
+                                        <small>12:49</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example5 Example5</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor sit amet.</p>
+                                        <small>1:15</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example6 Example6</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor...</p>
+                                        <small>21:11</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example6 Example6</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor...</p>
+                                        <small>21:11</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example6 Example6</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor...</p>
+                                        <small>21:11</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example6 Example6</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor...</p>
+                                        <small>21:11</small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
+                                <div class="friend-details">
+                                    <span>Example6 Example6</span>
+                                    <div class="flex-row">
+                                        <p>Lorem ipsum dolor...</p>
+                                        <small>21:11</small>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="chat-section">
+                        <div class="chat-messages">
+                            <h2>Hello</h2>
+                        </div>
+                        <div class="chat-toolbar">
+                            Toolbar
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 
 </div><?php }
 }
