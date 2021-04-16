c986db661d2db8c168b324b80102affb51cef07e
cserigabi4 <cserigabi4@gmail.com>
2021-04-16 20:00:12 +0200

    merge

--- Kozossegi/Teszt/templates_c/11cae28c8935cb5c66880e952afd25f6d037c378_0.file.profil.tpl.php
+++ Kozossegi/Teszt/templates_c/11cae28c8935cb5c66880e952afd25f6d037c378_0.file.profil.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-04-16 18:43:11
+/* Smarty version 3.1.39, created on 2021-04-16 19:46:18
   from '/opt/lampp/htdocs/Kozossego_Oldal/kozossegi_oldal/Kozossegi/Teszt/templates/profil.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_6079be9f265921_61170781',
+  'unifunc' => 'content_6079cd6a7cdff6_02965099',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '11cae28c8935cb5c66880e952afd25f6d037c378' => 
     array (
       0 => '/opt/lampp/htdocs/Kozossego_Oldal/kozossegi_oldal/Kozossegi/Teszt/templates/profil.tpl',
-      1 => 1618591294,
+      1 => 1618595101,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:menu.tpl' => 1,
   ),
 ),false)) {
-function content_6079be9f265921_61170781 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6079cd6a7cdff6_02965099 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 $_smarty_tpl->_subTemplateRender("file:menu.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
@@ -30,22 +30,25 @@ $_smarty_tpl->_subTemplateRender("file:menu.tpl", $_smarty_tpl->cache_id, $_smar
     <div class="profile-area">
         <div class="profile-container">
             <div class="profile-card col-md-11">
-                <div class="background-image">
+                                <div class="background-image">
                     <img src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="fejléc">
                 </div>
-                <div class="profile-image">
+
+                                <div class="profile-image">
                     <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="profilkép">
                 </div>
-                <div class="profile-main">
-                    <div class="profile-name">
+
+                                <div class="profile-main">
+                                        <div class="profile-name">
                         <h2>Example Example</h2>
                     </div>
-                    <div class="personal-details">
+                                        <div class="personal-details">
                         <span><i class="fas fa-envelope"></i>example@example.com</span>
                         <span><i class="fas fa-birthday-cake"></i>2000.08.05.</span>
                     </div>
-                    <div class="flex-row">
-                        <div class="profile-card more-details">
+
+                                        <div class="flex-row">
+                                                <div class="profile-card more-details">
                             <h3>Infos</h3>
                             <ul>
                                 <li><i class="fas fa-user"></i>Male (Gender)</li>
@@ -53,7 +56,7 @@ $_smarty_tpl->_subTemplateRender("file:menu.tpl", $_smarty_tpl->cache_id, $_smar
                                 <li><i class="fas fa-graduation-cap"></i>SZTE (University)</li>
                             </ul>
                         </div>
-                        <div class="profile-card profile-posting">
+                                                <div class="profile-card profile-posting">
                             <h3>Bejegyzés Írása</h3>
                             <div class="form-floating mt-3 mb-3">
                                 <textarea class="form-control" placeholder="Írjon bejegyzést" id="floatingTextarea2"></textarea>
@@ -69,6 +72,17 @@ $_smarty_tpl->_subTemplateRender("file:menu.tpl", $_smarty_tpl->cache_id, $_smar
                             </div>
                         </div>
                     </div>
+
+                                        <div class="profile-card post-card mx-auto">
+                        <div class="post-header">
+                            <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="profilkép">
+                            <div class="post-header-details">
+                                <span>Example example</span>
+                                <span class="small">12:32</span>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
