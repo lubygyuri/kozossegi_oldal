9690acbc69fbb8e32a85184ee6ee6db03b65ebdd
lubygyuri <lubigyurma@gmail.com>
2021-04-17 02:01:45 +0200

    #17 kommentelt tartalom

--- Kozossegi/Teszt/templates/uzenetek.tpl
+++ Kozossegi/Teszt/templates/uzenetek.tpl
@@ -5,101 +5,77 @@
     <div class="profile-area">
         <div class="profile-container">
             <div class="messages-card mx-auto">
+
+                {* Üzenetek fejléc *}
                 <div class="messages-header">
+                    {* Keresés a partnerek között *}
                     <form action="#" class="mx-auto">
                         <input type="search" placeholder="Search">
                         <button type="submit"><i class="fas fa-search"></i></button>
                     </form>
 
+                    {* Üzenetek fejlécének adatai *}
                     <div class="header-details-container">
+                        {* Profil név (azaz én) *}
                         <span>Example example</span>
+                        {* Profilkép *}
                         <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="profilkép">
                     </div>
                 </div>
+
+                {* Üzenetek main *}
                 <div class="messages-main">
+                    {* Barátok listázva *}
                     <div class="messages-friends-container">
                         <ul>
                             <li>
+                                {* Barát profilképe *}
                                 <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
+                                {* Barát adatai *}
                                 <div class="friend-details">
+                                    {* Barát neve *}
                                     <span>Example2 Example2</span>
                                     <div class="flex-row">
+                                        {* Barát utolsó üzenete *}
                                         <p>Lorem ipsum dolor...</p>
+                                        {* Barát utolsó üzenetének időpontja *}
                                         <small>14:34</small>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <img src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
-                                <div class="friend-details">
-                                    <span>Example3 Example3</span>
-                                    <div class="flex-row">
-                                        <p>Lorem ipsum dolor...</p>
-                                        <small>7:30</small>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <img src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
-                                <div class="friend-details">
-                                    <span>Example4 Example4</span>
-                                    <div class="flex-row">
-                                        <p>Lorem ipsum dolor sit.</p>
-                                        <small>12:49</small>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <img src="https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
-                                <div class="friend-details">
-                                    <span>Example5 Example5</span>
-                                    <div class="flex-row">
-                                        <p>Lorem ipsum dolor sit amet.</p>
-                                        <small>1:15</small>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
-                                <div class="friend-details">
-                                    <span>Example6 Example6</span>
-                                    <div class="flex-row">
-                                        <p>Lorem ipsum dolor...</p>
-                                        <small>21:11</small>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
-                                <div class="friend-details">
-                                    <span>Example6 Example6</span>
-                                    <div class="flex-row">
-                                        <p>Lorem ipsum dolor...</p>
-                                        <small>21:11</small>
                                     </div>
                                 </div>
                             </li>
                         </ul>
                     </div>
 
+                    {* Maga a chat *}
                     <div class="chat-section">
+                        {* Aktuális chat partner *}
                         <div class="chat-partner">
+                            {* Aktuális chat partner képe *}
                             <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
+                            {* Aktuális chat partner neve*}
                             <span>Example2 example2</span>
                         </div>
 
+                        {* Aktuális chaten megjelenő üzenetek *}
                         <div class="chat-messages">
+                            {* Bejövő üzenetek *}
                             <div class="incoming-messages">
+                                {* Maga a bejövő üzenet *}
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet deleniti ex iste iusto mollitia, nisi quis repellendus reprehenderit sunt unde?</p>
                             </div>
+                            {* Kimenő üzenetek *}
                             <div class="outgoing-messages">
+                                {* Maga a kimenő üzenet *}
                                 <p>Lorem ipsum dolor sit amet.</p>
                             </div>
                         </div>
 
+                        {* Eszköztár *}
                         <div class="chat-toolbar">
                             <form action="#">
+                                {* Ahova írhaod az üzeneted *}
                                 <input type="text" placeholder="Write something...">
+                                {* A gomb amivel elküldheted az üzeneted *}
                                 <button><i class="far fa-paper-plane"></i></button>
                             </form>
                         </div>
@@ -111,3 +87,55 @@
     </div>
 
 </div>
+
+{* Barátokra példa *}
+{*<li>*}
+{*    <img src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">*}
+{*    <div class="friend-details">*}
+{*        <span>Example3 Example3</span>*}
+{*        <div class="flex-row">*}
+{*            <p>Lorem ipsum dolor...</p>*}
+{*            <small>7:30</small>*}
+{*        </div>*}
+{*    </div>*}
+{*</li>*}
+{*<li>*}
+{*    <img src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">*}
+{*    <div class="friend-details">*}
+{*        <span>Example4 Example4</span>*}
+{*        <div class="flex-row">*}
+{*            <p>Lorem ipsum dolor sit.</p>*}
+{*            <small>12:49</small>*}
+{*        </div>*}
+{*    </div>*}
+{*</li>*}
+{*<li>*}
+{*    <img src="https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">*}
+{*    <div class="friend-details">*}
+{*        <span>Example5 Example5</span>*}
+{*        <div class="flex-row">*}
+{*            <p>Lorem ipsum dolor sit amet.</p>*}
+{*            <small>1:15</small>*}
+{*        </div>*}
+{*    </div>*}
+{*</li>*}
+{*<li>*}
+{*    <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">*}
+{*    <div class="friend-details">*}
+{*        <span>Example6 Example6</span>*}
+{*        <div class="flex-row">*}
+{*            <p>Lorem ipsum dolor...</p>*}
+{*            <small>21:11</small>*}
+{*        </div>*}
+{*    </div>*}
+{*</li>*}
+{*<li>*}
+{*    <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">*}
+{*    <div class="friend-details">*}
+{*        <span>Example6 Example6</span>*}
+{*        <div class="flex-row">*}
+{*            <p>Lorem ipsum dolor...</p>*}
+{*            <small>21:11</small>*}
+{*        </div>*}
+{*    </div>*}
+{*</li>*}
--- Kozossegi/Teszt/templates_c/384aab2ccffe3e3fa45ec699e3298a54df090614_0.file.uzenetek.tpl.php
+++ Kozossegi/Teszt/templates_c/384aab2ccffe3e3fa45ec699e3298a54df090614_0.file.uzenetek.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-04-17 01:39:21
+/* Smarty version 3.1.39, created on 2021-04-17 02:00:44
   from 'C:\xampp\htdocs\kozossegi_oldal\Kozossegi\Teszt\templates\uzenetek.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_607a20294d3a22_17903083',
+  'unifunc' => 'content_607a252c9cbc06_53116695',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '384aab2ccffe3e3fa45ec699e3298a54df090614' => 
     array (
       0 => 'C:\\xampp\\htdocs\\kozossegi_oldal\\Kozossegi\\Teszt\\templates\\uzenetek.tpl',
-      1 => 1618616359,
+      1 => 1618617640,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:menu.tpl' => 1,
   ),
 ),false)) {
-function content_607a20294d3a22_17903083 (Smarty_Internal_Template $_smarty_tpl) {
+function content_607a252c9cbc06_53116695 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 $_smarty_tpl->_subTemplateRender("file:menu.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
@@ -31,102 +31,54 @@ $_smarty_tpl->_subTemplateRender("file:menu.tpl", $_smarty_tpl->cache_id, $_smar
     <div class="profile-area">
         <div class="profile-container">
             <div class="messages-card mx-auto">
-                <div class="messages-header">
-                    <form action="#" class="mx-auto">
+
+                                <div class="messages-header">
+                                        <form action="#" class="mx-auto">
                         <input type="search" placeholder="Search">
                         <button type="submit"><i class="fas fa-search"></i></button>
                     </form>
 
-                    <div class="header-details-container">
-                        <span>Example example</span>
-                        <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="profilkép">
+                                        <div class="header-details-container">
+                                                <span>Example example</span>
+                                                <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="profilkép">
                     </div>
                 </div>
-                <div class="messages-main">
-                    <div class="messages-friends-container">
+
+                                <div class="messages-main">
+                                        <div class="messages-friends-container">
                         <ul>
                             <li>
-                                <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
-                                <div class="friend-details">
-                                    <span>Example2 Example2</span>
+                                                                <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
+                                                                <div class="friend-details">
+                                                                        <span>Example2 Example2</span>
                                     <div class="flex-row">
-                                        <p>Lorem ipsum dolor...</p>
-                                        <small>14:34</small>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <img src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
-                                <div class="friend-details">
-                                    <span>Example3 Example3</span>
-                                    <div class="flex-row">
-                                        <p>Lorem ipsum dolor...</p>
-                                        <small>7:30</small>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <img src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
-                                <div class="friend-details">
-                                    <span>Example4 Example4</span>
-                                    <div class="flex-row">
-                                        <p>Lorem ipsum dolor sit.</p>
-                                        <small>12:49</small>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <img src="https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="profilkép">
-                                <div class="friend-details">
-                                    <span>Example5 Example5</span>
-                                    <div class="flex-row">
-                                        <p>Lorem ipsum dolor sit amet.</p>
-                                        <small>1:15</small>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
-                                <div class="friend-details">
-                                    <span>Example6 Example6</span>
-                                    <div class="flex-row">
-                                        <p>Lorem ipsum dolor...</p>
-                                        <small>21:11</small>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
-                                <div class="friend-details">
-                                    <span>Example6 Example6</span>
-                                    <div class="flex-row">
-                                        <p>Lorem ipsum dolor...</p>
-                                        <small>21:11</small>
+                                                                                <p>Lorem ipsum dolor...</p>
+                                                                                <small>14:34</small>
                                     </div>
                                 </div>
                             </li>
                         </ul>
                     </div>
 
-                    <div class="chat-section">
-                        <div class="chat-partner">
-                            <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
-                            <span>Example2 example2</span>
+                                        <div class="chat-section">
+                                                <div class="chat-partner">
+                                                        <img src="https://images.unsplash.com/photo-1492681290082-e932832941e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="profilkép">
+                                                        <span>Example2 example2</span>
                         </div>
 
-                        <div class="chat-messages">
-                            <div class="incoming-messages">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet deleniti ex iste iusto mollitia, nisi quis repellendus reprehenderit sunt unde?</p>
+                                                <div class="chat-messages">
+                                                        <div class="incoming-messages">
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet deleniti ex iste iusto mollitia, nisi quis repellendus reprehenderit sunt unde?</p>
                             </div>
-                            <div class="outgoing-messages">
-                                <p>Lorem ipsum dolor sit amet.</p>
+                                                        <div class="outgoing-messages">
+                                                                <p>Lorem ipsum dolor sit amet.</p>
                             </div>
                         </div>
 
-                        <div class="chat-toolbar">
+                                                <div class="chat-toolbar">
                             <form action="#">
-                                <input type="text" placeholder="Write something...">
-                                <button><i class="far fa-paper-plane"></i></button>
+                                                                <input type="text" placeholder="Write something...">
+                                                                <button><i class="far fa-paper-plane"></i></button>
                             </form>
                         </div>
                     </div>
@@ -136,5 +88,7 @@ $_smarty_tpl->_subTemplateRender("file:menu.tpl", $_smarty_tpl->cache_id, $_smar
         </div>
     </div>
 
-</div><?php }
+</div>
+
+<?php }
 }
