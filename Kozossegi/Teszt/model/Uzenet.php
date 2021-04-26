4e14411e7ec23a648ec25af7e4a0e4d748c6e015
lubygyuri <lubigyurma@gmail.com>
2021-04-26 23:12:09 +0200

    adatbázis átírás

---
+++ Kozossegi/Teszt/model/Uzenet.php
@@ -0,0 +1,109 @@
+<?php
+
+
+class Uzenet {
+    private $azonosito;
+    private $uzenet;
+    private $kuldesIdeje;
+    private $kuldoAzonosito;
+    private $fogadoAzonosito;
+    private $kep;
+
+    /**
+     * @return mixed
+     */
+    public function getAzonosito()
+    {
+        return $this->azonosito;
+    }
+
+    /**
+     * @param mixed $azonosito
+     */
+    public function setAzonosito($azonosito)
+    {
+        $this->azonosito = $azonosito;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUzenet()
+    {
+        return $this->uzenet;
+    }
+
+    /**
+     * @param mixed $uzenet
+     */
+    public function setUzenet($uzenet)
+    {
+        $this->uzenet = $uzenet;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKuldesIdeje()
+    {
+        return $this->kuldesIdeje;
+    }
+
+    /**
+     * @param mixed $kuldesIdeje
+     */
+    public function setKuldesIdeje($kuldesIdeje)
+    {
+        $this->kuldesIdeje = $kuldesIdeje;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKuldoAzonosito()
+    {
+        return $this->kuldoAzonosito;
+    }
+
+    /**
+     * @param mixed $kuldoAzonosito
+     */
+    public function setKuldoAzonosito($kuldoAzonosito)
+    {
+        $this->kuldoAzonosito = $kuldoAzonosito;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFogadoAzonosito()
+    {
+        return $this->fogadoAzonosito;
+    }
+
+    /**
+     * @param mixed $fogadoAzonosito
+     */
+    public function setFogadoAzonosito($fogadoAzonosito)
+    {
+        $this->fogadoAzonosito = $fogadoAzonosito;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKep()
+    {
+        return $this->kep;
+    }
+
+    /**
+     * @param mixed $kep
+     */
+    public function setKep($kep)
+    {
+        $this->kep = $kep;
+    }
+
+    
+}
