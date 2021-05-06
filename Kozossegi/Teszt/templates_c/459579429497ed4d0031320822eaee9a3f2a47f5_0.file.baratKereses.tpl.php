d75330d3ffcc099e9b770342d27cdaedc87ada1a
cserigabi4 <cserigabi4@gmail.com>
2021-05-06 17:52:44 +0200

    kereses

--- Kozossegi/Teszt/templates_c/459579429497ed4d0031320822eaee9a3f2a47f5_0.file.baratKereses.tpl.php
+++ Kozossegi/Teszt/templates_c/459579429497ed4d0031320822eaee9a3f2a47f5_0.file.baratKereses.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-05-06 17:34:30
+/* Smarty version 3.1.39, created on 2021-05-06 17:52:14
   from '/opt/lampp/htdocs/Kozossegi/kozossegi_oldal/Kozossegi/Teszt/templates/baratKereses.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_60940c86aec961_79105408',
+  'unifunc' => 'content_609410ae96cc95_57482126',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '459579429497ed4d0031320822eaee9a3f2a47f5' => 
     array (
       0 => '/opt/lampp/htdocs/Kozossegi/kozossegi_oldal/Kozossegi/Teszt/templates/baratKereses.tpl',
-      1 => 1620315261,
+      1 => 1620316331,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_60940c86aec961_79105408 (Smarty_Internal_Template $_smarty_tpl) {
+function content_609410ae96cc95_57482126 (Smarty_Internal_Template $_smarty_tpl) {
 ?><div id="keresesDiv2">
     <ul id="keresesUl" class="list-group" style=" z-index: 1">
         <?php
@@ -28,7 +28,7 @@ $_smarty_tpl->tpl_vars['i'] = new Smarty_Variable(null, $_smarty_tpl->isRenderin
 if ($_smarty_tpl->tpl_vars['i']->total > 0) {
 for ($_smarty_tpl->tpl_vars['i']->value = 0, $_smarty_tpl->tpl_vars['i']->iteration = 1;$_smarty_tpl->tpl_vars['i']->iteration <= $_smarty_tpl->tpl_vars['i']->total;$_smarty_tpl->tpl_vars['i']->value += $_smarty_tpl->tpl_vars['i']->step, $_smarty_tpl->tpl_vars['i']->iteration++) {
 $_smarty_tpl->tpl_vars['i']->first = $_smarty_tpl->tpl_vars['i']->iteration === 1;$_smarty_tpl->tpl_vars['i']->last = $_smarty_tpl->tpl_vars['i']->iteration === $_smarty_tpl->tpl_vars['i']->total;?>
-            <li class="list-group-item list-group-item-dark"><a href="profil.php?email=<?php ob_start();
+            <li class="list-group-item list-group-item-dark"><a href="uzenetek.php?profil=<?php ob_start();
 echo $_smarty_tpl->tpl_vars['keresesEredmeny']->value[$_smarty_tpl->tpl_vars['i']->value]->getEmail();
 $_prefixVariable1 = ob_get_clean();
 echo $_prefixVariable1;?>
