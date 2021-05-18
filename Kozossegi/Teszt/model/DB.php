44012b982c8161a17f3e13fd415e5c62c697195d
lubygyuri <78864012+lubygyuri@users.noreply.github.com>
2021-05-18 19:45:36 +0200

    jelszo change

--- Kozossegi/Teszt/model/DB.php
+++ Kozossegi/Teszt/model/DB.php
@@ -4,7 +4,7 @@
 class DB {
     private $tns = "(DESCRIPTION = (ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521)))(CONNECT_DATA = (SERVICE_NAME = xe)))";
     private $db_username = "lublo";
-    private $db_password = "Xcsetr45";
+    private $db_password = "jelszo";
 
 
     protected function connect() {
